b71a03f5aca49813efb73af7db7bdf38e2d0bf53
pehu71 <humplikpetr@gmail.com>
2016-05-18 00:49:00 +0200

    Readme.md

--- copy.component.ts
+++ copy.component.ts
@@ -22,20 +22,20 @@ export class CopyComponent {
 
     copy() {
 
-        let selSpan = document.createElement('textarea');
+        let selBox = document.createElement('textarea');
 
-        selSpan.style.position = 'fixed';
-        selSpan.style.left = '0';
-        selSpan.style.top = '0';
-        selSpan.style.opacity = '0';
-        selSpan.value = this.text.join('\r\n');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = this.text.join('\r\n');
 
-        document.body.appendChild(selSpan);
-        selSpan.focus();
-        selSpan.select();
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
 
         document.execCommand('copy');
-        document.body.removeChild(selSpan);
+        document.body.removeChild(selBox);
     }
 
 }
